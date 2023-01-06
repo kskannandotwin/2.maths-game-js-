ce28cf7f609e71a75a52db6ba3e9e2cbd738be29
kskannandotwin <kskannan.win@gmail.com>
2023-01-06 06:07:35 +0530

    06.01.2023
    125. javascript code 3 has completed

--- javascript.js
+++ javascript.js
@@ -2,103 +2,123 @@ var playing = false;
 var score;
 var action;
 var timeremaining;
+var correctAnswer;
 
 // if we click on start/restart button
-document.getElementById('startreset').onclick = function() {
-    // if we are playing
-    if (playing == true) {
-        // reload page
-        location.reload();
-    } else {
-        // if we are not playing
-        // change mode to playing
-        playing = true;
-
-        // set scroe to 0
-        score = 0;
-        document.getElementById('scorevalue').innerHTML = score;
-
-        // show countdown box
-        show('timeremaining');
-        timeremaining = 60;
-        document.getElementById('timeremainingvalue').innerHTML = timeremaining;
-
-        // hide game over box
-        hide('gameOver');
-
-        // change button to reset
-        document.getElementById('startreset').innerHTML = 'Reset Game';
-
-        // start countdown
-        startCountdown();
-
-        // generate a new q&a
-        generateQA();
-    }
-
-    
-}
-    // if we are playing
-        // reload page
+document.getElementById("startreset").onclick = function () {
+  // if we are playing
+  if (playing == true) {
+    // reload page
+    location.reload();
+  } else {
     // if we are not playing
-        // set score to 0
-        // show count down box
-        // reduce time by 1 sec in loops
-            // time left?
-                // yes => continue
-                // no => game over
-        // change button to reset
-        // generate new q and a
+    // change mode to playing
+    playing = true;
+
+    // set scroe to 0
+    score = 0;
+    document.getElementById("scorevalue").innerHTML = score;
+
+    // show countdown box
+    show("timeremaining");
+    timeremaining = 60;
+    document.getElementById("timeremainingvalue").innerHTML = timeremaining;
+
+    // hide game over box
+    hide("gameOver");
+
+    // change button to reset
+    document.getElementById("startreset").innerHTML = "Reset Game";
+
+    // start countdown
+    startCountdown();
+
+    // generate a new q&a
+    generateQA();
+  }
+};
+// if we are playing
+// reload page
+// if we are not playing
+// set score to 0
+// show count down box
+// reduce time by 1 sec in loops
+// time left?
+// yes => continue
+// no => game over
+// change button to reset
+// generate new q and a
 
 // if we click on answer box
-    // if we are playing
-        // correct?
-            // yes
-                // increase score
-                // show correct box for 1sec
-                // generate new q and a
-            // no
-                // show try again box for 1sec
+// if we are playing
+// correct?
+// yes
+// increase score
+// show correct box for 1sec
+// generate new q and a
+// no
+// show try again box for 1sec
 
 // functions
 
 // start counter
 function startCountdown() {
-    action = setInterval(function() {
-        timeremaining -= 1;
-        document.getElementById('timeremainingvalue').innerHTML = timeremaining;
-        if (timeremaining == 0) {
-            stopCountdown();
-            show('gameOver');
-            document.getElementById('gameOver').innerHTML = '<p>Game over!</p><p>Your score is ' + score + '.</p>';
-            hide('timeremaining');
-            hide('correct');
-            hide('wrong');
-            playing = false;
-            document.getElementById('startreset').innerHTML = 'Start Game';
-        }
-    }, 1000);
+  action = setInterval(function () {
+    timeremaining -= 1;
+    document.getElementById("timeremainingvalue").innerHTML = timeremaining;
+    if (timeremaining == 0) {
+      stopCountdown();
+      show("gameOver");
+      document.getElementById("gameOver").innerHTML =
+        "<p>Game over!</p><p>Your score is " + score + ".</p>";
+      hide("timeremaining");
+      hide("correct");
+      hide("wrong");
+      playing = false;
+      document.getElementById("startreset").innerHTML = "Start Game";
+    }
+  }, 1000);
 }
 
 // stop counter
 function stopCountdown() {
-    clearInterval(action);
+  clearInterval(action);
 }
 
 // hide an element
 function hide(Id) {
-    document.getElementById(Id).style.display = 'none';
+  document.getElementById(Id).style.display = "none";
 }
 
 // show an element
 function show(Id) {
-    document.getElementById(Id).style.display = 'block';
+  document.getElementById(Id).style.display = "block";
 }
 
 // generate question and multiple answers
 function generateQA() {
+  var x = 1 + Math.round(9 * Math.random());
+  var y = 1 + Math.round(9 * Math.random());
+  correctAnswer = x * y;
+  document.getElementById("question").innerHTML = x + "x" + y;
+  var correctPosition = 1 + Math.round(3 * Math.random());
 
+  // fill 1 box with correct answer
+  document.getElementById("box" + correctPosition).innerHTML = correctAnswer;
+
+  //   fill other boxes with wrong answers
+  var answers = [correctAnswer];
+
+  for (i = 1; i < 5; i++) {
+    if (i != correctPosition) {
+      // a wrong answer
+      var wrongAnswer;
+      do {
+        wrongAnswer =
+          (1 + Math.round(9 * Math.random())) *
+          (1 + Math.round(9 * Math.random()));
+      } while (wrongAnswer == correctPosition);
+      document.getElementById("box" + i).innerHTML = wrongAnswer;
+    }
+  }  
 }
-
-
-
