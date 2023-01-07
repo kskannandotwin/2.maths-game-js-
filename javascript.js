97903dc9516697e82e3034090c97e87c08a00370
kskannandotwin <kskannan.win@gmail.com>
2023-01-07 07:03:46 +0530

    07.01.2023
    126. javascript code 4 has completed

--- javascript.js
+++ javascript.js
@@ -37,18 +37,39 @@ document.getElementById("startreset").onclick = function () {
     generateQA();
   }
 };
-// if we are playing
-// reload page
-// if we are not playing
-// set score to 0
-// show count down box
-// reduce time by 1 sec in loops
-// time left?
-// yes => continue
-// no => game over
-// change button to reset
-// generate new q and a
 
+// clicking an answer box
+for (i = 1; i < 5; i++ ) {
+  document.getElementById("box" + i).onclick = function () {
+    // check if we are playing
+    if (playing == true) {
+      // yes
+      if (this.innerHTML == correctAnswer) {
+        // correct answer
+        // increase score by 1
+        score++;
+        document.getElementById("scorevalue").innerHTML = score;
+        // hide wrong box and show correct box
+        hide("wrong");
+        show("correct");
+        setTimeout(function () {
+          hide("correct");
+        }, 1000);
+  
+        // generate new QA
+        generateQA();
+      } else {
+        // wrong answer
+        // hide correct box and show wrong box
+        hide("correct");
+        show("wrong");
+        setTimeout(function () {
+          hide("wrong");
+        }, 1000);
+      }
+    }
+  };
+}
 // if we click on answer box
 // if we are playing
 // correct?
@@ -117,8 +138,9 @@ function generateQA() {
         wrongAnswer =
           (1 + Math.round(9 * Math.random())) *
           (1 + Math.round(9 * Math.random()));
-      } while (wrongAnswer == correctPosition);
+      } while (answers.indexOf(wrongAnswer) > -1);
       document.getElementById("box" + i).innerHTML = wrongAnswer;
+      answers.push(wrongAnswer);
     }
-  }  
+  }
 }
